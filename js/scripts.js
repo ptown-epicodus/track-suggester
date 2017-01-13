2e4317f944dc9391c2d8d8f8bb2d936c02d8eb52
Patrick McGreevy <patrick7490@icloud.com>
2017-01-13 12:55:35 -0800

    Write function to compute results of quiz

--- js/scripts.js
+++ js/scripts.js
@@ -16,6 +16,101 @@ $(document).ready(function() {
       abstractionLevel = parseInt($("#low-high").val());
       license = $("input:radio[name=license]:checked").val();
 
+      calculateResult();
       event.preventDefault();
   });
 });
+
+
+var calculateResult = function() {
+  var ruby = 0;
+  var php = 0;
+  var java = 0;
+  var css = 0;
+  var cSharp = 0;
+
+  if (prefersProgramming) {
+    ruby += 1;
+    php += 1;
+    java += 1;
+    css -= 1;
+    cSharp += 1;
+  } else {
+    css += 5;
+  }
+
+  if (typingDiscipline == "strong") {
+    java += 1;
+    cSharp += 1;
+  } else if (typingDiscipline == "weak") {
+    ruby += 1;
+    php += 1;
+  }
+
+  switch(paradigm) {
+    case 'functional':
+      ruby += 1;
+      php += 1;
+      cSharp += 1;
+      break;
+    case 'generic':
+      java += 1;
+      cSharp += 1;
+      break;
+    case 'procedural':
+      php += 1;
+      break;
+    case 'structured':
+      java += 1;
+      cSharp += 1;
+      break;
+    case 'muli':
+      ruby += 1;
+      cSharp += 1;
+      break;
+    default:
+      break;
+  }
+
+  if (prefersSemicolon) {
+    php += 1;
+    java += 1;
+    css += 1;
+    cSharp += 1;
+  } else {
+    ruby += 1;
+  }
+
+  if (prefersCompilation) {
+    java += 1;
+    cSharp += 1;
+  } else {
+    ruby += 1;
+    php += 1;
+  }
+
+  switch(abstractionLevel) {
+    case 1:
+      css += 1;
+    case 2:
+      ruby += 1;
+    case 3:
+      php += 2;
+    case 4:
+      java += 1;
+    case 5:
+      cSharp += 1;
+      break;
+    default:
+      break;
+  }
+
+  if (license === "closed") {
+    java += 1;
+    cSharp += 1;
+  } else if (license === "open") {
+    ruby += 1;
+    php += 1;
+    css += 1;
+  }
+};
